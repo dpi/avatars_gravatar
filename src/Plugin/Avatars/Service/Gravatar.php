f3721e751a0751460701621b5d7afceedd5845b0
dpi <pro@danielph.in>
2017-12-16 21:22:17 +0800

    Added dpi/ak_gravatar common overrides.

---
+++ src/Plugin/Avatars/Service/Gravatar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\avatars_gravatar\Plugin\Avatars\Service;
+
+use Drupal\avatars\Plugin\Avatars\Service\AvatarKitCommonService;
+
+/**
+ * Gravatar plugin.
+ */
+class Gravatar extends AvatarKitCommonService { }
